ea66d4fa9c85751c7c7ae9fb767056cd828382bd
Nguyen Do Quoc Duy <duy.nguyenbkcs@hcmut.edu.vn>
2022-03-06 22:28:08 +0700

    Added tooltip

---
+++ Assets/Scripts/EditorScene/View/ShowTutorial.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+namespace EAR.View 
+{
+    public class ShowTutorial : MonoBehaviour
+    {
+        [SerializeField]
+        private Button openButton;
+        [SerializeField]
+        private Button closeButton;
+        [SerializeField]
+        private GameObject tutorialTable;
+
+        void Start()
+        {
+            openButton.onClick.AddListener(OpenTutorialTable);
+            closeButton.onClick.AddListener(CloseTutorialTable);
+            tutorialTable.transform.localScale = Vector3.zero;
+        }
+
+        private void OpenTutorialTable()
+        {
+            tutorialTable.transform.DOScale(Vector3.one, 0.2f);
+        }
+
+        private void CloseTutorialTable()
+        {
+            tutorialTable.transform.DOScale(Vector3.zero, 0.2f);
+        }
+    }
+}
+
